96cbac84212760b2216b89044bf1b7822131bd48
Sneha <86683361+snehab2@users.noreply.github.com>
2024-11-28 23:54:25 -0600

    updated button background color production please work

--- apps/web/src/components/Button/Button.tsx
+++ apps/web/src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ import './Button.css'
 function Button({buttonText, link} : {buttonText : String, link : any}) {
   return (
       <a href={link} className="text-xl" target="_blank">
-        <button className="primary-btn bg-[#909634] text-[#FFE9D7] w-full py-3 px-5">{buttonText}</button>
+        <button className="primary-btn bg-[#A6CDC4] text-[#FFE9D7] w-full py-3 px-5">{buttonText}</button>
       </a>
   )
 }
